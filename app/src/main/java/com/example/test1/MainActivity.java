9be55654084a620c5d57af5d066ae0dc4c20bc5e
void3996 <vHbYcnTPvxiqd2u>
2020-10-27 23:59:18 +0900

    test

---
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*https://programing-study.tistory.com/12?category=579832*/
+    }
+}
